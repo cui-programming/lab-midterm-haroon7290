b861756c767a31d3fad3f033fd01663e1ad896bc
haroon7290 <haroon7290@gmail.com>
2025-10-20 00:37:35 +0500

    Update TeacherMessage.js

--- src/components/custom/TeacherMessage.js
+++ src/components/custom/TeacherMessage.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { Text } from '../ui/index.js'; // ✅ import only Text from the UI layer
+
+export default function TeacherMessage() {
+  return (
+    <Text>
+      “Teacher Message: Perform Well!”
+      
+    </Text>
+   
+  );
+}
 
